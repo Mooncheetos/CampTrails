0a84b5e27be5ae757c4c8e312bbadbeee9cb06af
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-08-11 23:54:38 +0300

    ffourIn

---
+++ src/components/UI/index.js
@@ -0,0 +1,14 @@
+export { default as LoadButton } from "./LoadButton/LoadButton";
+export { default as LocationInput } from "./LocationInput/LocationInput";
+export { default as Category } from "./Category/Category";
+export { default as InvisibleTitle } from "./InvisibleTitle/InvisibleTitle";
+export { default as PageLoader } from "./PageLoader/PageLoader";
+export { default as Checkbox } from "./Checkbox/Checkbox";
+export { default as ReviewerRating } from "./ReviewerRating/ReviewerRating";
+export { default as BookInput } from "./BookInput/BookInput";
+export { default as Message } from "./Message/Message";
+export { default as Loader } from "./Loader/Loader";
+export { default as CamperImage } from "./CamperImage/CamperImage";
+export { default as LikeButton } from "./LikeButton/LikeButton";
+export { default as Rating } from "./Rating/Rating";
+export { default as Location } from "./Location/Location";
